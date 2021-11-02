d8d9d7fd350262013ebc014cdc6c855ee71c6ff4
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-02 19:06:22 -0300

    finaliza os exercicios do conteudo de objetos

---
+++ javascript-ES6/javascript-para-iniciantes/objetos/exercicio.js
@@ -0,0 +1,33 @@
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+var eu = {
+  nome: 'yuri',
+  sobrenome: 'garrido',
+  corFavorita: 'green',
+  dataDeNascimento: '06 de setembro de 2002',
+}
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+eu.nomeCompleto = function() {
+  return eu.nome + ' '+ eu.sobrenome;
+}
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
+var labrador = {
+  raca: 'labrador',
+  cor: 'preto',
+  idade: '10 anos',
+  late: 'sim',
+  gostaDeHomens: 'não'
+};
