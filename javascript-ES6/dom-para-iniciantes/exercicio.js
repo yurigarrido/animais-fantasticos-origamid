6931372fba968e07ef5f10b6fd7412388650e61c
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-03 23:49:42 -0300

    finaliza o exercicio sobre "dom para iniciantes" e baixa as imagens que serao usadas no projeto

---
+++ javascript-ES6/dom-para-iniciantes/exercicio.js
@@ -0,0 +1,11 @@
+// Retorne o url da página atual utilizando o objeto window
+const href = window.location.href;
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const primeiroElemento = document.querySelector('.ativo');
+
+// Retorne a linguagem do navegador
+const linguagem =  window.navigator.language;
+// Retorne a largura da janela 
+const width = window.innerWidth;
