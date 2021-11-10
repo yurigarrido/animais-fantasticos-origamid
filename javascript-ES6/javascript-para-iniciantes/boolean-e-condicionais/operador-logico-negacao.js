5ad5e9f264f81a2faba1a61d29d219acfcaf8262
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-10 19:40:13 -0300

    ajusta o setup do diretório e define escopo para realização de atividades

--- javascript-ES6/javascript-para-iniciantes/boolean-e-condicionais/operador-logico-negacao.js
+++
@@ -1,9 +0,0 @@
-// Truthy
-if(!true) // false
-if(!1) // false
-if(!'') // true
-if(!undefined) // true
-if(!!' ') // true
-if(!!'') // false
-
-// usando ! é possivel negar o valor que vem após ele.
